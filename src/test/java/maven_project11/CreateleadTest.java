ce669997f6cbc81bb4cc6dc4f8021016644a77b1
ravikumarbachu79 <ravikumarbachu79@gmail.com>
2022-11-17 11:57:58 +0530

    hh

--- src/test/java/maven_project11/CreateleadTest.java
+++ src/test/java/maven_project11/CreateleadTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class CreateleadTest {
 	@Test
 	public void leader() {
-		System.out.println("lead created");
+		System.out.println("lead created          ");
 	}
 
 }
